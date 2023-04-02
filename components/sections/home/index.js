3209d0b227bdf2a1ad97492fbd3771a57c07fa9f
Samet <46278098+imsamet@users.noreply.github.com>
2023-04-02 21:10:39 +0300

    run filter

--- components/sections/home/index.js
+++ components/sections/home/index.js
@@ -8,8 +8,45 @@ import ProductCard from '../../productCard';
 import Style from './style.module.scss';
 const HomeSection = () => {
   const [page, setPage] = useState(1);
-  const pages = Math.ceil(PRODUCTS?.length / 12)
-  console.log(PRODUCTS?.length / 12)
+  const [selectBrands, setSelectBrands] = useState([]);
+  const [selectModels, setSelectModels] = useState([]);
+  const [products, setProducts] = useState(PRODUCTS);
+  const pages = Math.ceil(products?.length / 12);
+  const bards = [];
+  const models = [];
+  PRODUCTS.map(i => {
+    !bards?.some(s => s === i?.brand) && bards.push(i?.brand);
+    !models?.some(s => s === i?.model) && models.push(i?.model);
+  });
+  const filterProduct = () => {
+    setProducts(
+      PRODUCTS.filter(i => (selectBrands.length > 0 ? selectBrands.some(s => s === i?.brand) : true)).filter(i =>
+        selectModels.length > 0 ? selectModels.some(s => s === i?.model) : true,
+      ),
+    );
+  };
+  const handleClickBrand = e => {
+    const newSelectedBrands = selectBrands;
+    if (newSelectedBrands.some(i => i === e)) {
+      const index = newSelectedBrands?.findIndex(x => x === e);
+      newSelectedBrands.splice(index, 1);
+    } else {
+      newSelectedBrands.push(e);
+    }
+    setSelectBrands(newSelectedBrands);
+    filterProduct();
+  };
+  const handleClickModel = e => {
+    const newSelectedModels = selectModels;
+    if (newSelectedModels.some(i => i === e)) {
+      const index = newSelectedModels?.findIndex(x => x === e);
+      newSelectedModels.splice(index, 1);
+    } else {
+      newSelectedModels.push(e);
+    }
+    setSelectModels(newSelectedModels);
+    filterProduct();
+  };
   return (
     <section className="section">
       <div className="container">
@@ -38,53 +75,33 @@ const HomeSection = () => {
               </div>
             </div>
             <div className={Style.filterContainer}>
-              <span>Brands</span>
-              <div className={Style.filterBox}>
-                <Input className={Style.search} />
-                <div className={Style.filterItem}>
-                  <Checkbox />
-                  <span>Old to new</span>
-                </div>
-                <div className={Style.filterItem}>
-                  <Checkbox />
-                  <span>New to old</span>
-                </div>
-                <div className={Style.filterItem}>
-                  <Checkbox />
-                  <span>Price hight to low</span>
-                </div>
-                <div className={Style.filterItem}>
-                  <Checkbox />
-                  <span>Price low to High</span>
-                </div>
-              </div>
-            </div>
-            <div className={Style.filterContainer}>
               <span>Model</span>
               <div className={Style.filterBox}>
                 <Input className={Style.search} />
-                <div className={Style.filterItem}>
-                  <Checkbox />
-                  <span>Old to new</span>
-                </div>
-                <div className={Style.filterItem}>
-                  <Checkbox />
-                  <span>New to old</span>
-                </div>
-                <div className={Style.filterItem}>
-                  <Checkbox />
-                  <span>Price hight to low</span>
-                </div>
-                <div className={Style.filterItem}>
-                  <Checkbox />
-                  <span>Price low to High</span>
-                </div>
+                {bards?.map(i => (
+                  <div key={i} className={Style.filterItem}>
+                    <Checkbox onChange={() => handleClickBrand(i)} />
+                    <span>{i}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className={Style.filterContainer}>
+              <span>Brands</span>
+              <div className={Style.filterBox}>
+                <Input className={Style.search} />
+                {models?.map(i => (
+                  <div key={i} className={Style.filterItem}>
+                    <Checkbox onChange={() => handleClickModel(i)} />
+                    <span>{i}</span>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
           <div className="col-8">
             <div className={Style.cardContainer}>
-              {PRODUCTS?.slice((page - 1) * 12, page * 12)?.map(i => (
+              {products?.slice((page - 1) * 12, page * 12)?.map(i => (
                 <ProductCard key={i?.id} img={i?.image} price={i?.price} title={i?.name} />
               ))}
             </div>
